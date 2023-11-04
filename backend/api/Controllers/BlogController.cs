a773a5174589d8cd06fc0891857da970637a5e71
s1kle <s1klem4@gmail.com>
2023-11-04 15:05:43 +1000

    Created RESTful blog controller

--- backend/api/Controllers/BlogController.cs
+++ backend/api/Controllers/BlogController.cs
@@ -1,7 +1,10 @@
 using AutoMapper;
-using BlogHub.Data.Commands.CreateBlog;
-using BlogHub.Data.Queries.GetBlog;
-using BlogHub.Data.Queries.GetBlogList;
+using AutoMapper.Configuration.Annotations;
+using BlogHub.Data.Commands.Create;
+using BlogHub.Data.Commands.Delete;
+using BlogHub.Data.Commands.Update;
+using BlogHub.Data.Queries.Get;
+using BlogHub.Data.Queries.GetList;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -48,10 +51,35 @@ public class BlogController : ControllerBase
     }
 
     [HttpPost]
-    public async Task<ActionResult<Guid>> Create([FromBody] CreateBlogModel createBlogDto)
+    public async Task<ActionResult<Guid>> Create([FromBody] CreateBlogDto dto)
     {
-        var mappedDto = _mapper.Map<CreateBlogCommand>(createBlogDto);
+        var mappedDto = _mapper.Map<CreateBlogCommand>(dto);
         var command = mappedDto with { UserId = _userId };
+
+        var blogId = await _mediator.Send(command);
+
+        return Ok(blogId);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<Guid>> Update(Guid id, [FromBody] UpdateBlogDto dto)
+    {
+        var mappedDto = _mapper.Map<UpdateBlogCommand>(dto);
+        var command = mappedDto with { Id = id, UserId = _userId };
+
+        var blogId = await _mediator.Send(command);
+
+        return Ok(blogId);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<Guid>> Delete(Guid id)
+    {
+        var command = new DeleteBlogCommand
+        {
+            Id = id,
+            UserId = _userId
+        };
 
         var blogId = await _mediator.Send(command);
 
